62a299a7be3849331c7adde773cb31cd0b48046c
thornicek <tomas.hornicek8@email.cz>
2025-01-26 19:26:30 -0500

    Manager added

--- Flaskr/db.py
+++ Flaskr/db.py
@@ -40,7 +40,7 @@ def init_db():
 
     with current_app.open_resource('schema.sql') as f:
         db.executescript(f.read().decode('utf8'))
-    add_manager(db, "admin", "1234")
+    add_manager(db, "Manager", "1234")
 
 @click.command('init-db')
 def init_db_command():
